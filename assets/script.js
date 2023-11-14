05f4722cf47b824f19cfdc02801ea510982f2816
unknown <dannysaravia100@gmail.com>
2023-11-13 20:49:17 -0500

    added ₿ symbol and background images to project

--- assets/script.js
+++ assets/script.js
@@ -31,8 +31,8 @@ function renderCryptoData(cryptoData) {
     const item = document.createElement('div');
     item.innerHTML = `
       <p>Name: ${asset.name}</p>
-      <p>Symbol: ${asset.symbol}</p>
-      <p>Price (USD): ${asset.priceUsd}</p>
+      <p>Symbol: ${asset.symbol} ₿ </p>
+      <p>Price (USD): ${asset.priceUsd} </p>
     `;
 
     cryptoDataContainer.appendChild(item);
